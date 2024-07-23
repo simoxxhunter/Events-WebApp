19d760a6de0f4a5eda08bfed04f7954467969f89
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-07-23 15:04:35 +0100

    Creation de UserService et UserServiceImpl

--- Hello_Events/src/main/java/com/mouad/Hello_Events/service/UserService.java
+++ Hello_Events/src/main/java/com/mouad/Hello_Events/service/UserService.java
@@ -1,2 +1,10 @@
-package com.mouad.Hello_Events.service;public interface UserService {
+package com.mouad.Hello_Events.service;
+
+import com.mouad.Hello_Events.model.User;
+
+import java.util.List;
+
+public interface UserService {
+     List<User> getAllUsers();
+     void deleteUser(Long id);
 }
