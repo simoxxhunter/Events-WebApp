f623eadb97d2040d524906f0e870693da912a17c
simoxxhunter <kingstonegame@gmail.com>
2024-07-24 17:50:49 +0100

    creation de module pour les membre de membre

---
+++ Hello_Events/src/main/java/com/mouad/Hello_Events/model/TeamMembers.java
@@ -0,0 +1,22 @@
+package com.mouad.Hello_Events.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TeamMembers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String position;
+    private String bio;
+    private String photoUrl;
+
+}
