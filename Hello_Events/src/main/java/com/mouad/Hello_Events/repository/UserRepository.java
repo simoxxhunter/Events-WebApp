910484e08b368bde9c5fdc0bb24444fa5d7e1f46
Mouaadlahmani <mouaadlahmani@gmail.com>
2024-07-26 15:28:08 +0100

    ajouter la fonction findByEmail et countAllUsers

--- Hello_Events/src/main/java/com/mouad/Hello_Events/repository/UserRepository.java
+++ Hello_Events/src/main/java/com/mouad/Hello_Events/repository/UserRepository.java
@@ -2,8 +2,16 @@ package com.mouad.Hello_Events.repository;
 
 import com.mouad.Hello_Events.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    @Query(value = "select count (*) from User")
+    Long countAllUsers();
 }
