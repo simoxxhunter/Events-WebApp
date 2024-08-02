5747967107454da0207d306495ba2897abae9298
simoxxhunter <kingstonegame@gmail.com>
2024-08-02 12:28:17 +0100

    final commit

--- Hello_Events/src/main/java/com/mouad/Hello_Events/model/enums/Role.java
+++ Hello_Events/src/main/java/com/mouad/Hello_Events/model/enums/Role.java
@@ -1,5 +1,12 @@
 package com.mouad.Hello_Events.model.enums;
 
-public enum Role {
-    ADMIN,USER
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ADMIN,USER,DIRECTEUR;
+
+    @Override
+    public String getAuthority() {
+        return "";
+    }
 }
